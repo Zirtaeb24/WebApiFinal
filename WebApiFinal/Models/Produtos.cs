03426bf79cabd4019a597b15f7f69b74f766d1fb
Zirtaeb24 <zirtaeb.silva.15@gmail.com>
2025-06-09 09:09:31 -0300

    cors

--- WebApiFinal/Models/Produtos.cs
+++ WebApiFinal/Models/Produtos.cs
@@ -7,5 +7,18 @@
         public string? Descricao { get; set; }
         public int Quantidade { get; set; }
         public int Valor { get; set; }
+
+        //tipos de dados
+
+        //string nome = "Etec"; //tipo de referêdcia
+        //char letra = 'E';
+
+        //int valor = 10; //números inteiros
+       // long valor1 = 10L; //números inteiros
+       // byte valorB = 127; //números inteiros
+
+       // double valorD = 10.5; //números reais
+       // float valorf = 10.5f; //números reais
+
     }
 }
--- WebApiFinal/Program.cs
+++ WebApiFinal/Program.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiFinal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<WebApiFinalContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFinalContext") ?? throw new InvalidOperationException("Connection string 'WebApiFinalContext' not found.")));
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Politicas de Cors Origin
+builder.Services.AddCors(EtecAll =>
+{
+    EtecAll.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+
+        }
+
+        );
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +36,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("EtecAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
